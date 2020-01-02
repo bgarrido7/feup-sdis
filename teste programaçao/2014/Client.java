bf5cd2ed5f05c60fc665729fd098c90c864b908a
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2020-01-02 13:14:18 +0000

    Add files via upload

---
+++ teste programaçao/2014/Client.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+
+public class Client {
+    private Socket skt;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public static void main(String args[]){
+        Client client = new Client(args[0], Integer.parseInt(args[1]));
+    }
+
+    public Client(String addr, int port){
+        try {
+            skt = new Socket(addr, port);
+
+            out = new DataOutputStream(skt.getOutputStream());
+            in = new DataInputStream(skt.getInputStream());
+//          in = new DataInputStream(new BufferedInputStream(skt.getInputStream()));
+
+            out.writeUTF("AVG");
+            System.out.println("message sent: AVG");
+
+           float avg = in.readFloat();
+           System.out.println("message received: " + avg);
+
+            //manda 1 msg e depois de receber fecha socket
+            in.close();
+            out.close();
+            skt.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+            System.exit(1);
+        }
+    }
+}
---
+++ teste programaçao/2014/Server.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    private ServerSocket ss;
+    private Socket srv;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public static void main(String args[]){
+        Server srv = new Server(Integer.parseInt(args[0]), Float.parseFloat(args[1]));
+    }
+
+    public Server(int port, float average){
+        try {
+            ss = new ServerSocket(port);
+
+            while(true) {
+                System.out.println("waiting for client's request...");
+                srv = ss.accept();
+
+                in = new DataInputStream(srv.getInputStream());
+                out = new DataOutputStream(srv.getOutputStream());
+
+                String msg = in.readUTF();
+
+                if(msg.equals("AVG")){
+                    System.out.println("message received: AVG");
+                    out.writeFloat(average);
+                    System.out.println("Sent message: " + average);
+                }
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
+         //   out.close();
+         //   ss.close();
+            System.exit(1);
+        }
+
+    }
+}
