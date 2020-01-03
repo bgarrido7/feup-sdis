c80558fb0255895aa34321d354d7792f0799318e
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2020-01-03 11:47:28 +0000

    organized things

--- código para o trabalho/costa - ServerTestExample/src/Server.java
+++ código para o trabalho/costa - ServerTestExample/src/Server.java
@@ -1,51 +1,45 @@
-import java.net.*;
 import java.io.*;
-
+import java.net.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
+    private ServerSocket ss;
+    private Socket srv;
+    private InputStream in;
+    private OutputStream out;
+    private int port = 5000;
 
+    private String currentDirectory = System.getProperty("user.dir");
 
+    public static void main(String args[]){
+        new Server();
+    }
 
+    public Server(){
+        System.out.println("waiting for client's request...");
         try {
-            serverSocket = new ServerSocket(4444);
-        } catch (IOException ex) {
-            System.out.println("Can't setup server on this port number. ");
+            File file = new File(currentDirectory + "/file/pp-2015.pdf");
+
+            ss = new ServerSocket(port);
+            srv = ss.accept();
+            in = new FileInputStream(file);
+            out = srv.getOutputStream();
+
+            byte[] bytes = new byte[16*1024];
+            int count;
+            while ((count = in.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
+            }
+            System.out.println("file sent!");
+
+            srv.close();
+            out.close();
+            in.close();
         }
 
-        Socket socket = null;
-        InputStream in = null;
-        OutputStream out = null;
-
-        try {
-            socket = serverSocket.accept();
-        } catch (IOException ex) {
-            System.out.println("Can't accept client connection. ");
+        catch(IOException e){
+            System.out.println(e);
+            System.exit(1);
         }
 
-        try {
-            in = socket.getInputStream();
-        } catch (IOException ex) {
-            System.out.println("Can't get socket input stream. ");
-        }
-
-        try {
-            out = new FileOutputStream("c:\\Testes\\receivefile\\receivefile.pdf");
-        } catch (FileNotFoundException ex) {
-            System.out.println("File not found. ");
-        }
-
-        byte[] bytes = new byte[16*1024];
-
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
-        }
-
-        out.close();
-        in.close();
-        socket.close();
-        serverSocket.close();
     }
 }
